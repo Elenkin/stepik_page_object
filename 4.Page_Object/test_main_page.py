db3a5d92ceab6240eab4ca63cf7d31d22f7fb771
e.pavlova <e.pavlova@bi.zone>
2020-11-09 16:40:52 +0300

    создана папка page и добавлены файлы: base_page.py и main_page.py

--- 4.Page_Object/test_main_page.py
+++ 4.Page_Object/test_main_page.py
@@ -1,7 +1,12 @@
 """ PyTest --tb=line, выводит только одну строку из лога каждого упавшего теста """
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login_page(browser)
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
