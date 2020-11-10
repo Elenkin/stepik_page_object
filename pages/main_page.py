85369a30169fd9997b4d768a0d51cd0d65b81d2c
e.pavlova <e.pavlova@bi.zone>
2020-11-10 20:22:29 +0300

    в BasePage: - добавлен timeout - реализован метод is_element_present - изменён метод проверки логина

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    # передаём два аргумента: как искать (css, id, xpath и тд) и что искать (строку-селектор)
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
